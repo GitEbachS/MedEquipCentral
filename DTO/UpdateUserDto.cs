aee221eb4aed7e4f195b2312dc89e40db88daf06
Shari Ebach <ebachshari@yahoo.com>
2024-06-16 20:24:23 -0500

    update

---
+++ DTO/UpdateUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MedEquipCentral.DTO
+{
+    public class UpdateUserDto
+    {
+        public string Image { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int JobFunctionId { get; set; }
+        public bool IsBizOwner { get; set; }
+
+    }
+}
